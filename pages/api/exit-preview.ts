0b8bec45162d7f996f36cf0600454647cd8482c6
OlegKomissarov <o.komissarov247@gmail.com>
2023-11-22 16:49:20 +0100

    minor

--- pages/api/exit-preview.ts
+++ pages/api/exit-preview.ts
@@ -1,4 +1,4 @@
-export default async function exit(req, res) {
+export default async function exitDraftMode(req, res) {
     const { slug = "" } = req.query;
     // Exit the current user from "Preview Mode". This function accepts no args.
     res.clearPreviewData();
