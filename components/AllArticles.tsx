70b69820e562026e9afd34ed4c89573848d8b392
OlegKomissarov <o.komissarov247@gmail.com>
2023-11-22 15:32:50 +0100

    Try to fix build err

--- components/AllArticles.tsx
+++ components/AllArticles.tsx
@@ -3,7 +3,7 @@ import { getStoryblokApi, storyblokEditable } from '@storyblok/react';
 
 import { useState, useEffect } from 'react';
 
-const AllArticles = ({ blok }: { blok: any }) => {
+const AllArticles = ({ blok }) => {
     const [articles, setArticles] = useState([]);
     useEffect(() => {
         const getArticles = async () => {
@@ -33,14 +33,17 @@ const AllArticles = ({ blok }: { blok: any }) => {
 
     // todo добавить .env
     return (
-        <div {...storyblokEditable(blok)}>
+        <>
             <p className="text-3xl">{blok.title}</p>
-            <div className="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3   lg:px-24 md:px-16">
+            <div
+                className="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3   lg:px-24 md:px-16"
+                {...storyblokEditable(blok)}
+            >
                 {articles[0] && articles.map((article) => (
                     <ArticleTeaser article={article.content} key={article.uuid} />
                 ))}
             </div>
-        </div>
+        </>
     );
 };
 export default AllArticles;
